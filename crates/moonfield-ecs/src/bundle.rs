2f2f588ff0617ee1559fc482607e45ae981574a3
furuoxuan <furuoxuan@lixiang.com>
2026-08-13 17:04:29 +0800

    feat(ecs): implement bundle traits for component tuples

--- crates/moonfield-ecs/src/bundle.rs
+++ crates/moonfield-ecs/src/bundle.rs
@@ -1,10 +1,26 @@
 use crate::{archetype::ComponentMeta, Component};
-use std::any::TypeId;
+use std::any::{type_name, TypeId};
+use std::fmt;
+use std::mem;
 use std::ptr::NonNull;
 
 /// Error indicating that an entity did not have a required component
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub struct MissingComponent(&'static str);
+
+impl MissingComponent {
+    pub fn new<T: Component>() -> Self {
+        Self(type_name::<T>())
+    }
+}
+
+impl fmt::Display for MissingComponent {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "missing {} component", self.0)
+    }
+}
+
+impl core::error::Error for MissingComponent {}
 
 /// A type-erased collection of components that can be inserted into an ECS
 /// archetype.
@@ -73,3 +89,118 @@ impl DynamicClone {
         }
     }
 }
+
+/// A dynamically typed collection of cloneable components
+///
+/// # Safety
+///
+/// The callback passed to [`Self::put_with_clone`] must be invoked with a
+/// pointer to a valid, initialized component value described by the
+/// accompanying [`ComponentMeta`], plus an appropriate [`DynamicClone`].
+pub unsafe trait DynamicBundleClone: DynamicBundle {
+    /// Allow a callback to move all components out of the bundle, cloning each
+    /// one with a type-erased [`DynamicClone`].
+    ///
+    /// # Safety
+    ///
+    /// See the trait-level contract.
+    unsafe fn put_with_clone(self, f: impl FnMut(*mut u8, ComponentMeta, DynamicClone));
+}
+
+macro_rules! tuple_impl {
+    ($($name:ident),*) => {
+        unsafe impl<$($name: Component),*> DynamicBundle for ($($name,)*) {
+            fn has<T: Component>(&self) -> bool {
+                false $(|| TypeId::of::<$name>() == TypeId::of::<T>())*
+            }
+
+            fn key(&self) -> Option<TypeId> {
+                Some(TypeId::of::<Self>())
+            }
+
+            fn with_ids<T>(&self, f: impl FnOnce(&[TypeId]) -> T) -> T {
+                Self::with_static_ids(f)
+            }
+
+            fn component_meta(&self) -> Vec<ComponentMeta> {
+                Self::with_static_component_meta(|info| info.to_vec())
+            }
+
+            #[allow(unused_variables, unused_mut)]
+            unsafe fn put(self, mut f: impl FnMut(*mut u8, ComponentMeta)) {
+                #[allow(non_snake_case)]
+                let ($(mut $name,)*) = self;
+                $(
+                    f(
+                        (&mut $name as *mut $name).cast::<u8>(),
+                        ComponentMeta::of::<$name>()
+                    );
+                    mem::forget($name);
+                )*
+            }
+        }
+
+        unsafe impl<$($name: Component + Clone),*> DynamicBundleClone for ($($name,)*) {
+            // Compiler false positive warnings
+            #[allow(unused_variables, unused_mut)]
+            unsafe fn put_with_clone(
+                self,
+                mut f: impl FnMut(*mut u8, ComponentMeta, DynamicClone),
+            ) {
+                #[allow(non_snake_case)]
+                let ($(mut $name,)*) = self;
+                $(
+                    f(
+                        (&mut $name as *mut $name).cast::<u8>(),
+                        ComponentMeta::of::<$name>(),
+                        DynamicClone::new::<$name>()
+                    );
+                    mem::forget($name);
+                )*
+            }
+        }
+
+        #[allow(clippy::zero_repeat_side_effects)]
+        unsafe impl<$($name: Component),*> Bundle for ($($name,)*) {
+            fn with_static_ids<T>(f: impl FnOnce(&[TypeId]) -> T) -> T {
+                const N: usize = count!($($name),*);
+                let mut xs: [(usize, TypeId); N] =
+                    [$((mem::align_of::<$name>(), TypeId::of::<$name>())),*];
+                xs.sort_unstable_by(|x, y| x.0.cmp(&y.0).reverse().then(x.1.cmp(&y.1)));
+                let mut ids = [TypeId::of::<()>(); N];
+                for (slot, &(_, id)) in ids.iter_mut().zip(xs.iter()) {
+                    *slot = id;
+                }
+                f(&ids)
+            }
+
+            fn with_static_component_meta<T>(f: impl FnOnce(&[ComponentMeta]) -> T) -> T {
+                const N: usize = count!($($name),*);
+                let mut xs: [ComponentMeta; N] = [$(ComponentMeta::of::<$name>()),*];
+                xs.sort_unstable();
+                f(&xs)
+            }
+
+            #[allow(unused_variables, unused_mut)]
+            unsafe fn get(
+                mut f: impl FnMut(ComponentMeta) -> Option<NonNull<u8>>,
+            ) -> Result<Self, MissingComponent> {
+                #[allow(non_snake_case)]
+                let ($(mut $name,)*) = ($(
+                    f(ComponentMeta::of::<$name>())
+                        .ok_or_else(MissingComponent::new::<$name>)?
+                        .as_ptr()
+                        .cast::<$name>(),)*
+                );
+                Ok(($($name.read(),)*))
+            }
+        }
+    };
+}
+
+macro_rules! count {
+    () => { 0 };
+    ($x:ident $(, $rest:ident)*) => { 1 + count!($($rest),*) };
+}
+
+smaller_tuples_too!(tuple_impl, O, N, M, L, K, J, I, H, G, F, E, D, C, B, A);
